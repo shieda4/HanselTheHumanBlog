e6c082d9fa909e01606d85e8cbb30e77988ff75e
shieda4 <shiedaiz10@gmail.com>
2021-07-04 13:22:44 +0800

    Fixed Content

--- articles/models.py
+++ articles/models.py
@@ -12,3 +12,6 @@ class Article(models.Model):
     date = models.DateTimeField(auto_now=True)
     image = models.ImageField()
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
