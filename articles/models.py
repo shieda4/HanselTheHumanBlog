56f44833b3b0df6fd725f21cb81161e5742206f9
shieda4 <shiedaiz10@gmail.com>
2021-07-07 09:21:00 +0800

    Fixed articles Ordering

--- articles/models.py
+++ articles/models.py
@@ -14,7 +14,7 @@ class Article(models.Model):
     content = models.TextField()
 
     class Meta:
-        ordering = ['-date']
+        ordering = ['date']
 
     def __str__(self):
         return self.title
